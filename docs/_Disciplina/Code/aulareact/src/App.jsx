2c4e55e589c11ae4a9584a6208745051733218dc
Jonh Carvalho <jonh.carvalho@professores.ibmec.edu.br>
2024-10-17 16:14:59 -0300

    updates

--- docs/_Disciplina/Code/aulareact/src/App.jsx
+++ docs/_Disciplina/Code/aulareact/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyButton from './MyButton.jsx'
+import MyButton from './components/Header/MyButton.jsx'
 
 
 function App() {
