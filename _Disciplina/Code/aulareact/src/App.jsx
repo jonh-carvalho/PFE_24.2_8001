078e8e4b889fc453730e6dc74f4da1fec0e989df
jonh-carvalho <'jonh.carvalho@professores.ibmec.edu.br'>
2024-10-21 09:29:41 -0300

    Deployed a280d6e with MkDocs version: 1.6.1

--- _Disciplina/Code/aulareact/src/App.jsx
+++ _Disciplina/Code/aulareact/src/App.jsx
@@ -1,19 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import MyButton from './MyButton.jsx'
-
+// import MyButton from './components/Header/MyButton.jsx'
+import Home from './components/Home.jsx'
+import About from './components/About.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+    /*<>
       <h1>Bem-vindo ao meu aplicativo</h1>
       <MyButton />
 
-    </>
+    </>*/
+
   )
 }
 
--- _Disciplina/Code/aulareact/src/MyButton.jsx
+++
@@ -1,9 +0,0 @@
-function MyButton() {
-    return (
-      <button>
-        Eu sou um botão
-      </button>
-    );
-  }
-
-  export default MyButton
